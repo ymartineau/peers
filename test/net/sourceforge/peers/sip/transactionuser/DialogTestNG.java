c924539f7fe16f65a8b64fc95b5ba1e18fb715e1
Yohann Martineau <yohann.martineau@gmail.com>
2009-09-21 22:09:01 +0000

    updated legal statements

--- test/net/sourceforge/peers/sip/transactionuser/DialogTestNG.java
+++ test/net/sourceforge/peers/sip/transactionuser/DialogTestNG.java
@@ -1,5 +1,5 @@
 /*
-    This file is part of Peers.
+    This file is part of Peers, a java SIP softphone.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
-    Copyright 2008 Yohann Martineau 
+    Copyright 2008, 2009 Yohann Martineau 
 */
 
 package net.sourceforge.peers.sip.transactionuser;
