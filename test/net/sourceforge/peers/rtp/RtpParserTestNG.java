d95c05d9e9aeb95c86edf48709c1c23fbcf8a18f
Yohann Martineau <yohann.martineau@gmail.com>
2010-04-20 07:11:16 +0000

    added copyright header

--- test/net/sourceforge/peers/rtp/RtpParserTestNG.java
+++ test/net/sourceforge/peers/rtp/RtpParserTestNG.java
@@ -1,3 +1,22 @@
+/*
+    This file is part of Peers, a java SIP softphone.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright 2010 Yohann Martineau 
+*/
+
 package net.sourceforge.peers.rtp;
 
 import org.testng.annotations.Test;
@@ -8,7 +27,6 @@ public class RtpParserTestNG {
     public void testDecode() {
         byte[] packet = new byte[172];
         int pos = 0;
-        // rtp header from peers/integration-tests/captures/test6.pcap packet 2
         packet[pos++] = new Integer(0x80).byteValue();
         packet[pos++] = new Integer(0x00).byteValue();
         packet[pos++] = new Integer(0x02).byteValue();
