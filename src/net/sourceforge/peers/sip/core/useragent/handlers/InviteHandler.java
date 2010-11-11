12f9afdbb87db0df28b8e87e21956980414a9a81
Yohann Martineau <yohann.martineau@gmail.com>
2010-11-11 22:20:43 +0000

    extract media management in its own class

--- src/net/sourceforge/peers/sip/core/useragent/handlers/InviteHandler.java
+++ src/net/sourceforge/peers/sip/core/useragent/handlers/InviteHandler.java
@@ -27,10 +27,6 @@ import java.util.Hashtable;
 import java.util.List;
 
 import net.sourceforge.peers.Logger;
-import net.sourceforge.peers.media.CaptureRtpSender;
-import net.sourceforge.peers.media.Echo;
-import net.sourceforge.peers.media.IncomingRtpReader;
-import net.sourceforge.peers.media.SoundManager;
 import net.sourceforge.peers.sdp.MediaDestination;
 import net.sourceforge.peers.sdp.NoCodecException;
 import net.sourceforge.peers.sdp.SessionDescription;
@@ -459,68 +455,8 @@ public class InviteHandler extends DialogMethodHandler
         String localAddress = userAgent.getConfig()
             .getLocalInetAddress().getHostAddress();
 
-        switch (userAgent.getMediaMode()) {
-        case captureAndPlayback:
-            CaptureRtpSender captureRtpSender;
-            //TODO this could be optimized, create captureRtpSender at stack init
-            //     and just retrieve it here
-            SoundManager soundManager = userAgent.getSoundManager();
-            soundManager.openAndStartLines();
-            try {
-                captureRtpSender = new CaptureRtpSender(localAddress,
-                        userAgent.getRtpPort(),
-                        remoteAddress, remotePort, soundManager,
-                        userAgent.isMediaDebug());
-            } catch (IOException e) {
-                Logger.error("input/output error", e);
-                return;
-            }
-            userAgent.setCaptureRtpSender(captureRtpSender);
-
-            try {
-                captureRtpSender.start();
-            } catch (IOException e) {
-                Logger.error("input/output error", e);
-            }
-            
-            IncomingRtpReader incomingRtpReader;
-            try {
-                //TODO retrieve port from SDP offer
-//                    incomingRtpReader = new IncomingRtpReader(localAddress,
-//                            Utils.getInstance().getRtpPort(),
-//                            remoteAddress, remotePort);
-                incomingRtpReader = new IncomingRtpReader(
-                        captureRtpSender.getRtpSession(), soundManager);
-            } catch (IOException e) {
-                Logger.error("input/output error", e);
-                return;
-            }
-            userAgent.setIncomingRtpReader(incomingRtpReader);
-
-            incomingRtpReader.start();
-            break;
-
-        case echo:
-            Echo echo;
-            try {
-                echo = new Echo(localAddress, userAgent.getRtpPort(),
-                            remoteAddress, remotePort);
-            } catch (UnknownHostException e) {
-                Logger.error("unknown host amongst "
-                        + localAddress + " or " + remoteAddress);
-                return;
-            }
-            userAgent.setEcho(echo);
-            Thread echoThread = new Thread(echo);
-            echoThread.start();
-            break;
-        case none:
-        default:
-            break;
-        }
-
-        
-        /////////////////
+        userAgent.getMediaManager().successResponseReceived(localAddress,
+                remoteAddress, remotePort);
         
         //switch to confirmed state
         dialog.receivedOrSent2xx();
@@ -616,82 +552,7 @@ public class InviteHandler extends DialogMethodHandler
         String destAddress = mediaDestination.getDestination();
         int destPort = mediaDestination.getPort();
         
-        switch (userAgent.getMediaMode()) {
-        case captureAndPlayback:
-            //TODO this could be optimized, create captureRtpSender at stack init
-            //     and just retrieve it here
-            CaptureRtpSender captureRtpSender;
-            captureRtpSender = userAgent.getCaptureRtpSender();
-            IncomingRtpReader incomingRtpReader =
-                userAgent.getIncomingRtpReader();
-            if (incomingRtpReader != null) {
-                incomingRtpReader.stop();
-            }
-            if (captureRtpSender != null) {
-                captureRtpSender.stop();
-                while (!captureRtpSender.isTerminated()) {
-                    try {
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        Logger.debug("sleep interrupted");
-                    }
-                }
-            }
-            SoundManager soundManager = userAgent.getSoundManager();
-            soundManager.closeLines();
-            soundManager.openAndStartLines();
-            try {
-                captureRtpSender = new CaptureRtpSender(userAgent.getConfig()
-                            .getLocalInetAddress().getHostAddress(),
-                        userAgent.getRtpPort(), destAddress, destPort,
-                        soundManager, userAgent.isMediaDebug());
-            } catch (IOException e) {
-                Logger.error("input/output error", e);
-                return;
-            }
-            userAgent.setCaptureRtpSender(captureRtpSender);
-            try {
-                captureRtpSender.start();
-            } catch (IOException e) {
-                Logger.error("input/output error", e);
-            }
-            try {
-                //TODO retrieve port from SDP offer
-//                        incomingRtpReader = new IncomingRtpReader(localAddress,
-//                                Utils.getInstance().getRtpPort(),
-//                                remoteAddress, remotePort);
-                //FIXME RTP sessions can be different !
-                incomingRtpReader = new IncomingRtpReader(
-                        captureRtpSender.getRtpSession(), soundManager);
-            } catch (IOException e) {
-                Logger.error("input/output error", e);
-                return;
-            }
-            userAgent.setIncomingRtpReader(incomingRtpReader);
-
-            incomingRtpReader.start();
-            break;
-        case echo:
-            Echo echo;
-            try {
-                echo = new Echo(userAgent.getConfig().getLocalInetAddress()
-                            .getHostAddress(),
-                        userAgent.getRtpPort(), destAddress, destPort);
-            } catch (UnknownHostException e) {
-                Logger.error("unknown host amongst "
-                        + userAgent.getConfig().getLocalInetAddress()
-                            .getHostAddress() + " or " + destAddress);
-                return;
-            }
-            userAgent.setEcho(echo);
-            Thread echoThread = new Thread(echo);
-            echoThread.start();
-            break;
-        case none:
-        default:
-            break;
-        }
-        
+        userAgent.getMediaManager().handleAck(destAddress, destPort);
     }
 
     public void transactionTimeout(ClientTransaction clientTransaction) {
