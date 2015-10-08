63f781ac78e1ae25d3d6e2e0c13698cb4dde671b
evrycollin <evry.collin@gmail.com>
2015-10-08 14:00:08 +0200

    cleanup

--- peers-lib/src/main/java/net/sourceforge/peers/media/DtmfFactory.java
+++ peers-lib/src/main/java/net/sourceforge/peers/media/DtmfFactory.java
@@ -46,49 +46,23 @@ public class DtmfFactory {
         data[2] = 0;
         // duration 8 bits
         data[3] = -96;
-
         RtpPacket rtpPacket = new RtpPacket();
         rtpPacket.setData(data);
         rtpPacket.setPayloadType(RFC4733.PAYLOAD_TYPE_TELEPHONE_EVENT);
         rtpPacket.setMarker(true);
         packets.add(rtpPacket);
 
-		/*
-        // two classical packets
-
-        rtpPacket = new RtpPacket();
-        // set duration to 320
-        data = data.clone();
-        data[2] = 1;
-        data[3] = 64;
-        rtpPacket.setData(data);
-        rtpPacket.setMarker(false);
-        rtpPacket.setPayloadType(RFC4733.PAYLOAD_TYPE_TELEPHONE_EVENT);
-        packets.add(rtpPacket);
-
-        rtpPacket = new RtpPacket();
-        // set duration to 320
-        data = data.clone();
-        data[2] = 1;
-        data[3] = -32;
-        rtpPacket.setData(data);
-        rtpPacket.setMarker(false);
-        rtpPacket.setPayloadType(RFC4733.PAYLOAD_TYPE_TELEPHONE_EVENT);
-        packets.add(rtpPacket);
-		*/
         data = data.clone();
         // create three end event packets
         data[1] = -0x76; // end event flag + volume set to 10
         // set Duration to 640
         data[2] = 2; // duration 8 bits
         data[3] = -128; // duration 8 bits
-        //for (int r = 0; r < 1; r++) {
-            rtpPacket = new RtpPacket();
-            rtpPacket.setData(data);
-            rtpPacket.setMarker(false);
-            rtpPacket.setPayloadType(RFC4733.PAYLOAD_TYPE_TELEPHONE_EVENT);
-            packets.add(rtpPacket);
-        //}
+		rtpPacket = new RtpPacket();
+		rtpPacket.setData(data);
+		rtpPacket.setMarker(false);
+		rtpPacket.setPayloadType(RFC4733.PAYLOAD_TYPE_TELEPHONE_EVENT);
+		packets.add(rtpPacket);
 
         return packets;
     }
