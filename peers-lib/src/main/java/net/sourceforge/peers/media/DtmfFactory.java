af1aab33a00c9b6022e925524edd6ac55e8db2a1
Yohann Martineau <yohann.martineau@nokia.com>
2016-10-22 00:01:52 +0200

    DTMF end packets should not increase timestamp
    closes #29

--- peers-lib/src/main/java/net/sourceforge/peers/media/DtmfFactory.java
+++ peers-lib/src/main/java/net/sourceforge/peers/media/DtmfFactory.java
@@ -86,6 +86,7 @@ public class DtmfFactory {
         for (int r = 0; r < 3; r++) {
             rtpPacket = new RtpPacket();
             rtpPacket.setData(data);
+            rtpPacket.setIncrementTimeStamp(false);
             rtpPacket.setMarker(false);
             rtpPacket.setPayloadType(RFC4733.PAYLOAD_TYPE_TELEPHONE_EVENT);
             packets.add(rtpPacket);
